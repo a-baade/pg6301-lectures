0a9ad386aaa2d9b642b1773228e20b6d52f3c59d
a-baade <andreasbaade@hotmail.com>
2022-01-17 11:47:24 +0100

    fetch movies from server

--- client/index.jsx
+++ client/index.jsx
@@ -72,7 +72,10 @@ function NewMovie({moviesApi}) {
 function Application() {
     const moviesApi ={
         onAddMovie: async (m) => MOVIES.push(m),
-        listMovies: async () => MOVIES
+        listMovies: async () => {
+            const res = await fetch("/api/movies");
+            return res.json();
+        }
     }
 
     return <BrowserRouter>
