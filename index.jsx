e0e1bbf8a1b4a944aec747733cdd3e8bacfc9827
a-baade <andreasbaade@hotmail.com>
2022-01-17 11:20:12 +0100

    Server React HTML built via parcel

--- index.jsx
+++ index.jsx
@@ -4,19 +4,6 @@ import {Routes, Route, Link, BrowserRouter, useNavigate} from "react-router-dom"
 import {useState, useEffect} from "react";
 
 
-const MOVIES = [
-    {
-        title: "The Matrix",
-        plot: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
-        year: 1999
-    },
-    {
-        title: "The Color Purple",
-        plot: "A black Southern woman (Whoopi Goldberg) struggles to find her identity after suffering years of abuse from her father and others over 40 years.",
-        year: 1985
-    }
-];
-
 function ListMovies({moviesApi}) {
     const [movies, setMovies] = useState();
     useEffect(async () => {
